d3dd4ad78bf999c3f3b88b659bfc9f75bf40ffc0
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-07-29 10:10:12 +0200

    remove unused data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,6 @@ export const load = async () => {
     const articles = await ndk.getAllArticles()
 
     return {
-        title: 'A list of blog posts',
         meta: {
             title: "A list of blog posts",
             description: 'This blog is an experiment. It uses Nostr as a CMS for all my articles.'
